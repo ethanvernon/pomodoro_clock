3c0c3c9b178cc25c43183d6dfb2c3c2d31d8f7fc
Ethan Vernon <ethan.vernon@colorado.edu>
2019-02-01 16:21:25 +0800

    Updates

---
+++ precache-manifest.b04435ac0f83a61a38fbce4bbac4a378.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2e34916ee51b2163f7b2ddd60b0fc90",
+    "url": "/pomodoro_clock/static/media/bell.e2e34916.mp3"
+  },
+  {
+    "revision": "7557d0226ffe5d22100b",
+    "url": "/pomodoro_clock/static/js/runtime~main.7557d022.js"
+  },
+  {
+    "revision": "ae9c77ac9003a0cc2ca8",
+    "url": "/pomodoro_clock/static/js/main.ae9c77ac.chunk.js"
+  },
+  {
+    "revision": "ef1b17ab6fb68f7bd93e",
+    "url": "/pomodoro_clock/static/js/1.ef1b17ab.chunk.js"
+  },
+  {
+    "revision": "ae9c77ac9003a0cc2ca8",
+    "url": "/pomodoro_clock/static/css/main.a2d364f2.chunk.css"
+  },
+  {
+    "revision": "ef1b17ab6fb68f7bd93e",
+    "url": "/pomodoro_clock/static/css/1.008443de.chunk.css"
+  },
+  {
+    "revision": "35990abe13e09ba872f70c984bd42819",
+    "url": "/pomodoro_clock/index.html"
+  }
+];
